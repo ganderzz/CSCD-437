206a829a30f3b1b4d4737c64755fdb1520832524
Dylan <stolen3@gmail.com>
2015-09-24 11:16:27 -0700

    Initial setup of assignment 1 (Self Replicating Program)

---
+++ Assignment 1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argv, char ** argc) {
+
+	printf("Self Replicating Program (Assignment 1)\n");
+	printf("CSCD 437 - 8am\n\n");
+	printf("By:\tRyan Babcock \n\tStand Bozhinov\n\tDylan Paulus\n");
+	printf("----------------------------\n");
+	return 0;
+
+}
