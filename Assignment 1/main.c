55d7b85a5f0ae73d65deb895b79ff7e3d264d1cb
Dylan <stolen3@gmail.com>
2015-09-24 19:14:13 -0700

    Replicating Program V2

--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -1,48 +1,191 @@
+char input[] = {
+	'#',
+	'i',
+	'n',
+	'c',
+	'l',
+	'u',
+	'd',
+	'e',
+	' ',
+	'<',
+	's',
+	't',
+	'd',
+	'i',
+	'o',
+	'.',
+	'h',
+	'>',
+	'\n',
+	'\n',
+	'i',
+	'n',
+	't',
+	' ',
+	'm',
+	'a',
+	'i',
+	'n',
+	'(',
+	')',
+	'\n',
+	'{',
+	'\n',
+	'\t',
+	'i',
+	'n',
+	't',
+	' ',
+	'i',
+	';',
+	'\n',
+	'\t',
+	'p',
+	'r',
+	'i',
+	'n',
+	't',
+	'f',
+	'(',
+	'"',
+	'c',
+	'h',
+	'a',
+	'r',
+	' ',
+	'i',
+	'n',
+	'p',
+	'u',
+	't',
+	'[',
+	']',
+	' ',
+	'=',
+	' ',
+	'{',
+	'\\',
+	'n',
+	'"',
+	')',
+	';',
+	'\n',
+	'\t',
+	'f',
+	'o',
+	'r',
+	' ',
+	'(',
+	'i',
+	' ',
+	'=',
+	' ',
+	'0',
+	';',
+	' ',
+	'i',
+	' ',
+	'<',
+	' ',
+	's',
+	'i',
+	'z',
+	'e',
+	'o',
+	'f',
+	'(',
+	'i',
+	'n',
+	'p',
+	'u',
+	't',
+	')',
+	';',
+	' ',
+	'i',
+	'+',
+	'+',
+	')',
+	'\n',
+	'\t',
+	'\t',
+	'p',
+	'r',
+	'i',
+	'n',
+	't',
+	'f',
+	'(',
+	'"',
+	'\\',
+	't',
+	'%',
+	'd',
+	',',
+	'\\',
+	'n',
+	'"',
+	',',
+	' ',
+	'i',
+	'n',
+	'p',
+	'u',
+	't',
+	'[',
+	'i',
+	']',
+	')',
+	';',
+	'\n',
+	'\t',
+	'p',
+	'r',
+	'i',
+	'n',
+	't',
+	'f',
+	'(',
+	'"',
+	'}',
+	';',
+	'\\',
+	'n',
+	'%',
+	's',
+	'"',
+	',',
+	' ',
+	'i',
+	'n',
+	'p',
+	'u',
+	't',
+	')',
+	';',
+	'\n',
+	'\t',
+	'r',
+	'e',
+	't',
+	'u',
+	'r',
+	'n',
+	' ',
+	'0',
+	';',
+	'\n',
+	'}'
+};
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
-* CSCD 437 - Secure Coding
-* Assignment 1
-* -------------
-* Ryan Babcock
-* Stan Bozhinov
-* Dylan Paulus
-* -------------
-*
-* Notes:
-*	Pretty sure this isn't what Tom wants, but here is a pretty basic self replicating program -Dylan
-*/
-
-int main(int argv, char ** argc) {
-
-	printf("Self Replicating Program (Assignment 1)\n");
-	printf("CSCD 437 - 8am\n\n");
-	printf("By:\tRyan Babcock \n\tStand Bozhinov\n\tDylan Paulus\n");
-	printf("----------------------------\n");
-
-	FILE * in, * out;
-	char * str = (char *)calloc(60, sizeof(char));
-
-	in = fopen("./main.c", "r");
-	out = fopen("./main-replicated.c", "w");
-
-	if( in != NULL || out != NULL)
-	{
-		while( fgets(str, 60, in) )
-		{
-			printf("%s", str);
-			fprintf(out, "%s", str);
-		}
-	}
-	else {
-		printf("ERROR: There was an error loading, or writing to a file.");
-	}
-
-	fclose(in);
-	fclose(out);
-
-	free(str);
-
+int main()
+{
+	int i;
+	printf("char input[] = {\n");
+	for (i = 0; i < sizeof(input); i++)
+		printf("\t%d,\n", input[i]);
+	printf("};\n%s", input);
 	return 0;
 }
