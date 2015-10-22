03867aa60bf9feaadfde71f2a0adbebecca7fcd5
ryanbabcock <ryanjbabcock@gmail.com>
2015-10-22 08:54:28 -0700

    Updated Java code
    BAM

--- Assignment_6/PimpMyInput.java
+++ Assignment_6/PimpMyInput.java
@@ -5,22 +5,31 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class PimpMyInput {
 
    public static void main(String...args) {
    
-      int a,b;
+      final String MAX_INT = "4294967295";
+      
+      int a=0,
+          b=0;
       Integer c;
       BufferedReader br = null;
       File input,
            output,
            error = new File("errLog.txt");
       FileReader fr = null;
-      FileWriter fw = null;
-      FileWriter fe = null;
+      FileWriter fw = null,
+                 fe = null;
+      Matcher m = null;
+      Pattern pattern = null;
       String firstName = "",
              lastName = "",
+             firstNum = "",
+             secondNum = "",
              password = "",
              rePassword = "",
              inputFile = "",
@@ -29,18 +38,51 @@ public class PimpMyInput {
              errors = "";
       Scanner kb = new Scanner(System.in);
 
-      System.out.print("Please enter your first name: ");
-      firstName = kb.nextLine();
-      
-      System.out.print("Please enter your last name: ");
-      lastName = kb.nextLine();
-      
-      System.out.print("Please enter a number: ");
-      a = kb.nextInt();
-      
-      System.out.print("Please enter another number: ");
-      b = kb.nextInt();
-      
+      try {
+         pattern = Pattern.compile("[A-Za-z]{1,50}");
+         System.out.print("Please enter your first name: ");
+         firstName = kb.nextLine();
+         m = pattern.matcher(firstName);
+         if(!(m.matches())) {
+            throw new IllegalArgumentException("First name invalid");
+         }
+         System.out.print("Please enter your last name: ");
+         lastName = kb.nextLine();
+         m = pattern.matcher(lastName);
+         if(!(m.matches())) {
+            throw new IllegalArgumentException("Last name invalid");
+         }
+      }
+      catch(IllegalArgumentException e) {
+         errors += e + "\n";
+      }
+      try {
+         pattern = Pattern.compile("[0-9]{1,10}");
+         System.out.print("Please enter a number: ");
+         firstNum = kb.nextLine();
+         m = pattern.matcher(firstNum);
+         if(!(m.matches())) {
+            throw new IllegalArgumentException("First number invalid");
+         }
+         if(firstNum.compareTo(MAX_INT) > 0) {
+            throw new IllegalArgumentException("First number too large");
+         }
+         a = Integer.parseInt(firstNum);
+         
+         System.out.print("Please enter another number: ");
+         secondNum = kb.nextLine();
+         m = pattern.matcher(secondNum);
+         if(!(m.matches())) {
+            throw new IllegalArgumentException("Second number invalid");
+         }
+         if(firstNum.compareTo(MAX_INT) > 0) {
+            throw new IllegalArgumentException("Second number too large");
+         }
+         b = Integer.parseInt(secondNum);
+      }
+      catch(IllegalArgumentException e) {
+         errors += e + "\n";
+      }
       System.out.print("Please enter the name of your input file: ");
       inputFile = kb.nextLine();
       
@@ -62,10 +104,10 @@ public class PimpMyInput {
          br = new BufferedReader(fr);
       }
       catch(FileNotFoundException e) {
-         errors += e.toString();
+         errors += e.toString() + "\n";
       }
       catch(IOException e) {
-         errors += e.toString();
+         errors += e.toString() + "\n";
       }
 
       try {         
@@ -86,7 +128,7 @@ public class PimpMyInput {
          while(!buffer.equals(null));
       }
       catch(Exception e) {
-         errors += e.toString();
+         errors += e.toString() + "\n";
       }
       try {
          fe.write(errors);
@@ -95,7 +137,7 @@ public class PimpMyInput {
          fr.close();
       }
       catch(IOException e) {
-         System.out.println(e);
+         System.out.println("An error occurred\n");
       }
    }
 }
