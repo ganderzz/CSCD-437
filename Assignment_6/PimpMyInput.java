f35f3bc3f5d67f0d003ae53f3aeefb8b0910702c
ryanbabcock <ryanjbabcock@gmail.com>
2015-10-29 09:12:43 -0700

    Hack My Java Bitches
    Sloppy java code, but seems to work fine.

--- Assignment_6/PimpMyInput.java
+++ Assignment_6/PimpMyInput.java
@@ -5,139 +5,279 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+
 
 public class PimpMyInput {
 
    public static void main(String...args) {
    
-      final String MAX_INT = "4294967295";
+      final String MAX_INT = ((Integer) Integer.MAX_VALUE).toString();
+      final String MIN_INT = ((Integer) Integer.MIN_VALUE).toString();
       
-      int a=0,
-          b=0;
-      Integer c;
-      BufferedReader br = null;
-      File input,
-           output,
-           error = new File("errLog.txt");
-      FileReader fr = null;
-      FileWriter fw = null,
-                 fe = null;
+      BigInteger a,b,c;
+      BufferedReader brEndfile = null,
+                     brPassword = null;
+      File inputFile,
+           outputFile,
+           errorFile = new File("errLog.txt"),
+           passwordFile = new File("password.txt");
+      FileReader frEndfile = null,
+                 frPassword = null;
+      FileWriter fwEndfile = null,
+                 fwErrorFile = null,
+                 fwPassword = null;
       Matcher m = null;
+      MessageDigest md = null;
       Pattern pattern = null;
+      SecureRandom sr = new SecureRandom();
       String firstName = "",
              lastName = "",
              firstNum = "",
              secondNum = "",
+             pw_hashed = "",
              password = "",
+             pw_filename = "pw.txt",
              rePassword = "",
-             inputFile = "",
-             outputFile = "",
+             input = "",
+             output = "",
              buffer = "",
-             errors = "";
+             err_string = "",
+             password_pattern = "(^(?=.*[A-Z]+)(?=.*[a-z]+)(?=.*[0-9]+)"+
+                           "(?=.*[,.;:!?]+))([a-zA-Z0-9,.;:!?]{8,32}$)",
+             seed = "";
       Scanner kb = new Scanner(System.in);
-
+      
+      // Names
+      while(true) {
+         try {
+            pattern = Pattern.compile("[A-Za-z]{1,50}");
+            System.out.print("Please enter your first name: ");
+            firstName = kb.nextLine();
+            m = pattern.matcher(firstName);
+            if(!(m.matches())) {
+               throw new IllegalArgumentException("First name invalid");
+            }
+            System.out.print("Please enter your last name: ");
+            lastName = kb.nextLine();
+            m = pattern.matcher(lastName);
+            if(!(m.matches())) {
+               throw new IllegalArgumentException("Last name invalid");
+            }
+            break;
+         }
+         catch(IllegalArgumentException e) {
+            err_string += e + "\n";
+         }
+      }//endwhile
+      
+      //Integers
+      while(true) {
+         try {
+            pattern = Pattern.compile("^-?[0-9]{1,10}$");
+            System.out.print("Please enter a number (no decimals): ");
+            firstNum = kb.nextLine();
+            m = pattern.matcher(firstNum);
+            if(!(m.matches())) {
+               throw new IllegalArgumentException("First number invalid");
+            }
+            if(firstNum.length() == MAX_INT.length()) {
+               if(firstNum.compareTo(MAX_INT) > 0) {
+                  throw new IllegalArgumentException("First number too large");
+               }
+            }
+            else if(firstNum.length() > MAX_INT.length()) {
+               if(firstNum.length() == MIN_INT.length() && firstNum.charAt(0) == '-') {
+                  if(firstNum.compareTo(MIN_INT) > 0) {
+                     throw new IllegalArgumentException("First number too small");
+                  }
+               }
+               else
+                  throw new IllegalArgumentException("First number too large");
+            }
+            a = new BigInteger(firstNum);
+            
+            System.out.print("Please enter another number: ");
+            secondNum = kb.nextLine();
+            m = pattern.matcher(secondNum);
+            if(!(m.matches())) {
+               throw new IllegalArgumentException("Second number invalid");
+            }
+            if(secondNum.length() == MAX_INT.length()) {
+               if(secondNum.compareTo(MAX_INT) > 0) {
+                  throw new IllegalArgumentException("First number too large");
+               }
+            }
+            else if(secondNum.length() > MAX_INT.length()) {
+               if(secondNum.length() == MIN_INT.length() && secondNum.charAt(0) == '-') {
+                  if(secondNum.compareTo(MIN_INT) > 0) {
+                     throw new IllegalArgumentException("Second number too small");
+                  }
+               }
+               else
+                  throw new IllegalArgumentException("Second number too large");
+            }
+            b = new BigInteger(secondNum);
+            break;
+         }
+         catch(IllegalArgumentException e) {
+            err_string += e + "\n";
+         }
+      }//endwhile
+      
+      // I/O Files
+      while(true) {
+         try {
+            pattern = Pattern.compile("[a-zA-Z0-9_-]{1,32}(\\.txt)$");
+            System.out.print("Please enter the name of your input file: ");
+            input = kb.nextLine();
+            m = pattern.matcher(input);
+            if(!m.matches()) {
+               throw new IllegalArgumentException("Input file name invalid");
+            }
+            System.out.print("Please enter the name of your output file: ");
+            output = kb.nextLine();
+            m = pattern.matcher(output);
+            if(!m.matches() || input.equalsIgnoreCase(output)) {
+               throw new IllegalArgumentException("Output file name invalid");
+            }
+            outputFile = new File(output);
+            inputFile = new File(input);
+            frEndfile = new FileReader(inputFile);
+            break;
+         }
+         catch(IllegalArgumentException e) {
+            err_string += e + "\n";
+            System.out.println("Invalid file name");
+         }
+         catch(IOException e) {
+            err_string += e + "\n";
+            System.out.println("No such file exists");
+         }
+      }//endwhile
+      
+      // MD Algorithm Catch
       try {
-         pattern = Pattern.compile("[A-Za-z]{1,50}");
-         System.out.print("Please enter your first name: ");
-         firstName = kb.nextLine();
-         m = pattern.matcher(firstName);
-         if(!(m.matches())) {
-            throw new IllegalArgumentException("First name invalid");
-         }
-         System.out.print("Please enter your last name: ");
-         lastName = kb.nextLine();
-         m = pattern.matcher(lastName);
-         if(!(m.matches())) {
-            throw new IllegalArgumentException("Last name invalid");
-         }
+         md = MessageDigest.getInstance("SHA-256");
       }
-      catch(IllegalArgumentException e) {
-         errors += e + "\n";
+      catch(NoSuchAlgorithmException e) {
+         err_string += e + "\n";
       }
+      // Password
+      while(true) {
+         try {
+            pattern = Pattern.compile(password_pattern);
+            System.out.print("Please enter your password (between 8-32 characters,"+
+                              "at least one capital,\nlowercase, number, and"+
+                              " punctuation mark: ");
+            password = kb.nextLine();
+            m = pattern.matcher(password);
+            if(!m.matches()) {
+               throw new IllegalArgumentException("Password invalid");
+            }
+            seed = new String(sr.generateSeed(20));
+            password = seed + password;
+            md.update(password.getBytes());
+            pw_hashed = new String(md.digest());
+            pw_hashed.replaceAll("\n", "");
+            break;
+         }
+         catch(Exception e) {
+            err_string += e + "\n";
+         }
+      }//endwhile
+      
+      //Store Password
       try {
-         pattern = Pattern.compile("[0-9]{1,10}");
-         System.out.print("Please enter a number: ");
-         firstNum = kb.nextLine();
-         m = pattern.matcher(firstNum);
-         if(!(m.matches())) {
-            throw new IllegalArgumentException("First number invalid");
-         }
-         if(firstNum.compareTo(MAX_INT) > 0) {
-            throw new IllegalArgumentException("First number too large");
-         }
-         a = Integer.parseInt(firstNum);
-         
-         System.out.print("Please enter another number: ");
-         secondNum = kb.nextLine();
-         m = pattern.matcher(secondNum);
-         if(!(m.matches())) {
-            throw new IllegalArgumentException("Second number invalid");
-         }
-         if(firstNum.compareTo(MAX_INT) > 0) {
-            throw new IllegalArgumentException("Second number too large");
-         }
-         b = Integer.parseInt(secondNum);
-      }
-      catch(IllegalArgumentException e) {
-         errors += e + "\n";
-      }
-      System.out.print("Please enter the name of your input file: ");
-      inputFile = kb.nextLine();
-      
-      System.out.print("Please enter the name of your output file: ");
-      outputFile = kb.nextLine();
-      
-      System.out.print("Please enter your password: ");
-      password = kb.nextLine();
-      
-      System.out.print("Please re-enter your password: ");
-      rePassword = kb.nextLine();
-      
-      output = new File(outputFile);
-      input = new File(inputFile);
-      try {
-         fe = new FileWriter(error);
-         fr = new FileReader(input);
-         fw = new FileWriter(output);
-         br = new BufferedReader(fr);
-      }
-      catch(FileNotFoundException e) {
-         errors += e.toString() + "\n";
+         fwPassword = new FileWriter(new File(pw_filename));
+         fwPassword.write(pw_hashed + "\n" + seed);
+         fwPassword.close();
       }
       catch(IOException e) {
-         errors += e.toString() + "\n";
+         System.out.println(e);
       }
+      //Re-Enter Password
+      while(true) {
+         try {
+            System.out.print("Please re-enter your password: ");
+            password = kb.nextLine();
+            m = pattern.matcher(password);
+            if(!m.matches()) {
+               throw new IllegalArgumentException("Password invalid");
+            }
+            frPassword = new FileReader(pw_filename);
+            brPassword = new BufferedReader(frPassword);
+            rePassword = brPassword.readLine();
+            seed = brPassword.readLine();
 
+            password = seed + password;
+            md.update(password.getBytes());
+            pw_hashed = new String(md.digest());
+            pw_hashed.replaceAll("\n", "");
+
+            if(!(pw_hashed.equals(rePassword))) {
+               throw new Exception("Passwords do not match!");
+            }
+            break;
+         }
+         catch(Exception e) {
+            err_string += e + "\n";
+            System.out.println("P1: " + rePassword + "\nP2: " + pw_hashed);
+         }
+      }//endwhile
+      try {
+         brPassword.close();
+      } catch(IOException e) {
+         err_string += e + "\n";
+      }
+      // Open readers
+      try {
+         fwErrorFile = new FileWriter(errorFile);
+         fwEndfile = new FileWriter(outputFile);
+         brEndfile = new BufferedReader(frEndfile);
+      }
+      catch(FileNotFoundException e) {
+         err_string += e.toString() + "\n";
+      }
+      catch(IOException e) {
+         err_string += e.toString() + "\n";
+      }
+      // Write to output file
       try {         
-         fw.write(firstName);
-         fw.write(lastName);
-         c = a + b;
-         fw.write(c.toString());
-         c = a * b;
-         fw.write(c.toString());
-      
+         fwEndfile.write(firstName + "\n");
+         fwEndfile.write(lastName + "\n");
+         c = a;
+         c = c.add(b);
+         fwEndfile.write(c.toString() + "\n");
+         c = a;
+         c = c.multiply(b);
+         fwEndfile.write(c.toString() + "\n");
+
          do {
-            buffer = br.readLine();
-            if(buffer.equals(null)) {
+            buffer = brEndfile.readLine();
+            if(buffer == null) {
                break;
             }
-            fw.write(buffer);
+            fwEndfile.write(buffer + "\n");
          }
          while(!buffer.equals(null));
       }
       catch(Exception e) {
-         errors += e.toString() + "\n";
+         err_string += e.toString() + "\n";
       }
       try {
-         fe.write(errors);
-         fe.close();
-         fw.close();
-         fr.close();
+         fwErrorFile.write(err_string);
+         fwErrorFile.close();
+         fwEndfile.close();
+         frEndfile.close();
       }
       catch(IOException e) {
-         System.out.println("An error occurred\n");
+         System.out.println("An errorFile occurred\n");
       }
    }
 }
